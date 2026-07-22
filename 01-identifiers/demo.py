9aa595f7c0ea8ea01e09d10c066ec339b5472f32
ravi2krishna <ravi2krishna@gmail.com>
2026-07-22 09:08:33 +0530

    class 3 - Identifier Rules / Syntax In Python

---
+++ 01-identifiers/demo.py
@@ -0,0 +1,48 @@
+# This is Comment - Python Ignores This Line 
+# run code # SyntaxError: invalid syntax
+
+print()
+print(10)
+print(10.5)
+
+# print(five) # NameError: name 'five' is not defined
+
+print('five')
+print("ten")
+
+# Identifiers 
+# 2day = "wednesday"
+# print(2day)
+
+today = "wednesday"
+print(today)
+
+day2moro = "thursday"
+print(day2moro)
+
+_2moro = "thursday"
+print(_2moro)
+
+# $2moro = "thursday"
+# print($2moro)
+
+# Improper Way of Naming Identifiers (Not Recommended)
+x = "Ravi"
+y = 25
+z = 9.3
+
+# Proper Way of Naming Identifiers (Recommended)
+# student name = "Ravi"
+student_name = "Ravi"
+student_age = 25
+student_gpa = 9.3
+
+# Static Data (Fixed / Doesn't Change)
+pi = 3.14159265359 # Not Recommended
+print(pi)
+
+PI = 3.14159265359 # Recommended
+print(PI)
+
+student_aadhar_id = 1234567890123 # Not Recommended
+STUDENT_AADHAR_ID = 1234567890123 # Recommended
