9838c3e94cb6093a523402eb862a197ffe81f192
ravi2krishna <ravi2krishna@gmail.com>
2026-07-27 09:05:46 +0530

    Class - 7 - Type Conversion & Type Casting

--- 04-data_types/demo.py
+++ 04-data_types/demo.py
@@ -48,3 +48,46 @@ print(type(data))
 # Dictionary 
 data = {"course":"python","duration":30}
 print(type(data))
+
+# Custom Data Type For Students 
+class Student:
+    student_id = 101
+    student_name = "Ravi"
+    student_email = "ravi2krishna@gmail.com"
+    student_enrolled_courses = ["Python","DevOps","Cloud"]
+    student_enrolled_courses_prices = (10000,25000,15000)
+    
+data = Student() # OOP -> Object Creation 
+print(type(data))
+
+# Type Conversion / Implicit Conversion [Automatic] 
+n1 = 10 # int 
+n2 = 5.5 # float 
+sum = n1 + n2 # float 
+print(sum)
+print(type(sum))
+
+# Type Casting / Explicit Conversion [Manual]
+price = 1150.12 
+print(price)
+print(type(price))
+
+# Round off price 
+price = int(price) # Type Casting
+print(price)
+print(type(price))
+
+# Some User in a web site was filling some form (text boxes) 
+# Behind the scenes these are strings
+
+rating = "1"
+print(type(rating))
+
+# if rating >= 4: # TypeError: '>=' not supported between instances of 'str' and 'int'
+rating = int(rating)
+if rating >= 4:
+    print("Positive Feedback") 
+else:
+    print("Negative Feedback") 
+
+
