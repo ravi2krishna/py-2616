03fa548284686135bea6d1412ec6eb080faf569c
ravi2krishna <ravi2krishna@gmail.com>
2026-08-06 09:13:02 +0530

    class 15 - Lists In Python

---
+++ 07_lists/demo.py
@@ -0,0 +1,90 @@
+# Lists 
+
+# empty list 
+empty_list = []
+print(empty_list)
+print(type(empty_list))
+
+empty_list = list()
+print(empty_list)
+print(type(empty_list))
+
+# List with Numeric Data 
+data = [10,20,30,40,50]
+print(data)
+
+# List with Text Data 
+data = ["python","ai","cloud"]
+print(data)
+
+# List with Mixed Data 
+data = [10,20,30,"python","ai",7.8,True]
+print(data)
+
+# Accessing Data In Lists
+data = [10,20,30,40,50]
+print(data)
+
+# first element 
+first_element = data[0]
+print(first_element)
+
+# last element 
+last_element = data[-1]
+print(last_element)
+
+# unknown element 
+# unknown_element = data[10] # IndexError: list index out of range
+# print(unknown_element)
+
+# Slicing In Lists same as strings 
+data = [10,20,30,40,50]
+print(data)
+print(data[1:3:1])# 20, 30
+print(data[0:5:2])# 10,30,50
+
+# Access Individual Elements
+data = [10,20,30,40,50]
+print(data[0])
+print(data[1])
+print(data[2])
+print(data[3])
+print(data[4])
+
+print("=" * 20)
+
+# Access Individual Elements -> 50k elements 
+data = [10,20,30,40,50000]
+# print(dir(data)) # __iter__
+for num in data:
+    print(num)
+
+print("=" * 20)
+
+# Apply Operators -> Requirements: Multiply Each Number with 10
+data = [10,20,30,40,50]
+for num in data:
+    print(num * 10)
+    
+print("=" * 20)
+
+# Apply Conditions -> Requirements: Give Only Even Numbers
+data = [10,20,35,40,55]
+for num in data:
+    if num % 2 == 0:
+        print(num)
+    
+print("=" * 20)
+
+# Duplicates Allowed 
+data = [10,20,10,30,40,10,50]
+print(data)
+
+print("=" * 20)
+
+# Insertion Order Is Preserved 
+data = [10,20,10,30,40,10,50]
+print(data)
+
+# List Operations / Methods 
+print(dir(data))
---
+++ 07_lists/ops.py
@@ -0,0 +1,91 @@
+# List Methods / Operations
+
+# append(): Add Element To end of the list
+data = [10,20,30,40,50]
+print(data)
+data.append(60)
+print(data)
+
+# extend(): Add Iterables To end of the list
+data = [10,20,30,40,50]
+print(data)
+data.extend([60,70,80])
+print(data)
+
+# insert(): add element at specific index position
+data = [10,20,40,50]
+print(data)
+data.append(30)
+print(data)
+
+data = [10,20,40,50]
+print(data)
+data.insert(2,30)
+print(data)
+
+# pop(): Remove element by default at last position
+data = [10,20,30,40,50]
+print(data)
+data.pop()
+print(data)
+
+# remove 30
+data = [10,20,30,40,50]
+print(data)
+data.pop(2)
+print(data)
+
+data = [10,20,30,40,50]
+print(data)
+# data.pop(20) # IndexError: pop index out of range
+print(data)
+
+# remove(): Remove element based on value
+data = [10,20,30,40,50]
+print(data)
+data.remove(30)
+print(data)
+
+# clear(): Remove all elements and empty list 
+data = [10,20,30,40,50]
+print(data)
+data.clear()
+print(data)
+
+# index(): Gives Index Position Of Value
+data = [10,20,30,40,50]
+print(data)
+data.index(40)
+print(data.index(40))
+# print(data.index(400)) # ValueError: 400 is not in list
+
+# count(): Return number of occurrences of value.
+data = [10,20,30,40,50]
+print(data)
+data.count(10)
+print(data.count(10))
+data = [10,20,10,30,40,10,50]
+print(data.count(10))
+
+# reverse() - Reverses list 
+data = [10,20,30,40,50]
+print(data)
+data.reverse()
+print(data)
+
+# sort(): Sort the list, default is ascending order 
+data = [10,20,30,50,40]
+print(data)
+data.sort()
+print(data)
+
+data = [10,20,30,50,40]
+print(data)
+data.sort(reverse=True)
+print(data)
+
+# copy(): Creates Copy Of List 
+data = [10,20,30,40,50]
+print(data)
+backup = data.copy()
+print(backup)
